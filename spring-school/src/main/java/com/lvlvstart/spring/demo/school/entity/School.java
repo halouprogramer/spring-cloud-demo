d96ca56c3b898c5c2a265d9e22048ecfa8c44018
吕子树 <zishu.lv@baodanyun-inc.com>
2019-12-09 17:57:25 +0800

    添加id

--- spring-school/src/main/java/com/lvlvstart/spring/demo/school/entity/School.java
+++ spring-school/src/main/java/com/lvlvstart/spring/demo/school/entity/School.java
@@ -3,6 +3,7 @@ package com.lvlvstart.spring.demo.school.entity;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 /**
  * @author zishu.lv@baodanyun-inc.com
@@ -13,6 +14,7 @@ import javax.persistence.Entity;
 @Entity
 public class School {
 
+    @Id
     private String id;
 
     /**
