14d1f022622aff32ec5730d82e568b2f49a66037
lzs <halouworld@126.com>
2019-12-17 23:51:36 +0800

    添加config项目

---
+++ spring-config/src/test/java/com/lvlvstart/spring/demo/config/SpringConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvlvstart.spring.demo.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
