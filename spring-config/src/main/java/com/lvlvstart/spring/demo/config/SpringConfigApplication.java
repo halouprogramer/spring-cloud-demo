843af64ecbf6c4b4d908a9aee14a85d0269ca0fc
lzs <halouworld@126.com>
2019-12-18 22:10:12 +0800

    搭建配置中心

--- spring-config/src/main/java/com/lvlvstart/spring/demo/config/SpringConfigApplication.java
+++ spring-config/src/main/java/com/lvlvstart/spring/demo/config/SpringConfigApplication.java
@@ -2,7 +2,11 @@ package com.lvlvstart.spring.demo.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
+@EnableConfigServer
 @SpringBootApplication
 public class SpringConfigApplication {
 
--- spring-school/src/main/java/com/lvlvstart/spring/demo/school/SpringSchoolApplication.java
+++ spring-school/src/main/java/com/lvlvstart/spring/demo/school/SpringSchoolApplication.java
@@ -2,9 +2,11 @@ package com.lvlvstart.spring.demo.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableEurekaClient
 public class SpringSchoolApplication {
 
