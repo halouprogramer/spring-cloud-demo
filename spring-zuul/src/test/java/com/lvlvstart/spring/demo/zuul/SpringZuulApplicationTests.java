fb38fbf29b77d6c9e281a8cbc9fa1943a13ab93d
lzs <halouworld@126.com>
2019-12-30 22:35:58 +0800

    搭建spring zuul ，并解决乱码

---
+++ spring-zuul/src/test/java/com/lvlvstart/spring/demo/zuul/SpringZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvlvstart.spring.demo.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
