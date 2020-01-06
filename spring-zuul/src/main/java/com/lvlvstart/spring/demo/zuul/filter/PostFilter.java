7bc919c912b147be4f696095172c6f4bff082871
吕子树 <zishu.lv@baodanyun-inc.com>
2020-01-06 19:19:06 +0800

    提交内容

--- spring-zuul/src/main/java/com/lvlvstart/spring/demo/zuul/filter/PostFilter.java
+++ spring-zuul/src/main/java/com/lvlvstart/spring/demo/zuul/filter/PostFilter.java
@@ -1,21 +1,19 @@
 package com.lvlvstart.spring.demo.zuul.filter;
 
-import com.netflix.ribbon.proxy.annotation.Http;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
-import org.springframework.http.HttpStatus;
-import sun.net.httpserver.AuthFilter;
+import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+@Component
 public class PostFilter extends ZuulFilter{
 
     /**
      * 处理器类型
-     * @return
+     * @return {@link FilterConstants#POST_TYPE}
      */
     @Override
     public String filterType() {
@@ -24,7 +22,7 @@ public class PostFilter extends ZuulFilter{
 
     /**
      * 执行顺序
-     * @return
+     * @return {@link FilterConstants#SEND_RESPONSE_FILTER_ORDER} -1
      */
     @Override
     public int filterOrder() {
@@ -44,6 +42,10 @@ public class PostFilter extends ZuulFilter{
     @Override
     public Object run() throws ZuulException {
 
+
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletResponse response = requestContext.getResponse();
+        response.setHeader("hello","hello");
         return null;
 
     }
