e43b9d0a9798a1961299dcb8fbadae7418d6c206
吕子树 <zishu.lv@baodanyun-inc.com>
2020-01-14 13:45:08 +0800

    配置hystrix 面板 还未完全成功

--- spring-user/src/main/java/com/lvlvstart/spring/demo/user/SpringUserApplication.java
+++ spring-user/src/main/java/com/lvlvstart/spring/demo/user/SpringUserApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients("com.lvlvstart.spring.demo.common.client")
+@EnableHystrix
+@EnableHystrixDashboard
 @EnableCircuitBreaker //熔断器注解
 public class SpringUserApplication {
 
